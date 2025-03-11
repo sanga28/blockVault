cce77a20943888b20b99e3a6c2decedf97136db8
Sanga_Bhattacharjee <sangabhattacharjee544@gmail.com>
2025-03-12 00:35:40 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,9 +3,9 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    hardhat: {}, // In-memory blockchain
+    hardhat: {}, 
     localhost: {
-      url: "http://127.0.0.1:8545", // Must match your Hardhat node URL
+      url: "http://127.0.0.1:8545", 
     },
   },
 };
